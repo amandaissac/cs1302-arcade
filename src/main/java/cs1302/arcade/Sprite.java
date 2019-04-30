45899ee62654f19d708716641ab21ae20a8333b0
Shreya Sastry <srs93414@uga.edu>
2019-04-30 17:04:04 -0400

    Sprite class is irrelevent

--- src/main/java/cs1302/arcade/Sprite.java
+++ src/main/java/cs1302/arcade/Sprite.java
@@ -1,8 +1,9 @@
 package cs1302.arcade;import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
 import javafx.scene.image.ImageView;
-public class Sprite extends Rectangle{
-    ImageView image;
+import javafx.scene.image.Image;
+public class Sprite extends ImageView{
+    //Sprite image;
     boolean alive = true;
     String type;
     Rectangle r;
@@ -13,23 +14,29 @@ public class Sprite extends Rectangle{
         this.type=type;
 	
         if(type.equals("player")){
-            image = new ImageView("https://i.pinimg.com/originals/4f/58/27/4f58272fa91d49ad60c22bf"+
-				  "49b94f3fb.webp");
+            
+                this.setImage(new Image("https://i.pinimg.com/originals/4f/58/27/4f58272fa91d49ad60c22bf"+
+                                        "49b94f3fb.webp"));      
+            //image = new ImageView("https://i.pinimg.com/originals/4f/58/27/4f58272fa91d49ad60c22bf"+
+            // "49b94f3fb.webp");
     
         }
         else if(type.equals("alien")){
-            image = new ImageView("https://i.pinimg.com/564x/34/7d/80/347d80a7c7cc0faf3a507a04a8d5"+
-				  "0433.jpg");
+            //image = new ImageView("https://i.pinimg.com/564x/34/7d/80/347d80a7c7cc0faf3a507a04a8d5"+
+			//	  "0433.jpg");
         }
         else{
-	    image= new ImageView("https://i.pinimg.com/564x/8a/34/04/8a340499a281be7b9166ecb"+
-				 "f81a49b3f.jpg");
+            //image= new ImageView("https://i.pinimg.com/564x/8a/34/04/8a340499a281be7b9166ecb"+
+            //               "f81a49b3f.jpg");
         }
-	image.setFitWidth(width);
-	image.setFitHeight(height);
+	this.setFitWidth(width);
+	this.setFitHeight(height);
     
     }
-
+    /*
+    public ImageView getImage(){
+        return image;
+        }*/
     /**
      *This method gives the type of tje sprite.
      *@return type of sprite
