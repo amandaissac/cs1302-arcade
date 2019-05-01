fbcc2c2925040781e0873a05749949c300cf46e7
Shreya Sastry <srs93414@uga.edu>
2019-05-01 11:12:02 -0400

    added shooting methods

--- src/main/java/cs1302/arcade/SpaceInvaders.java
+++ src/main/java/cs1302/arcade/SpaceInvaders.java
@@ -99,7 +99,33 @@ public class SpaceInvaders{
 	 for(int i=0;i<listEnemy.size();i++){
 	     listEnemy.get(i).setY(listEnemy.get(i).getY()+10);
 	 }
-      }
+     }
+    public int getXCoord(ImageView sprite){
+      return sprite.getX();
+    }
+    public int getYCoord(ImageView sprite){
+        return sprite.getY();
+    }
+    public void bulletAnim(ImageView player,ImageView bullet){
+        EventHandler<ActionEvent> handler = event -> {
+            updateBull(player,bullet);
+            
+        };
+        KeyFrame keyFrame = new KeyFrame(Duration.seconds(1), handler);
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.play();
+      
+    }
+    public void updateBull(ImageView player,ImageView bullet){
+        bullet.setX(getXCoord(player));
+        bullet.setY(getYCoord(player));
+        for(int i=0;i<listEnemy.size();i++){
+            bullet.setX(bullet.getY()+10);
+        }
+        
+    }
     
     /*
     public void alienMovement(){
